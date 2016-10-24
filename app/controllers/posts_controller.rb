d6eb6894f2bc3679626a7c7b2c86c4bd3d0a552a
mal-wood <mallorywood413@gmail.com>
2016-10-23 21:42:58 -0700

    Add show page along with create and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,26 @@ class PostsController < ApplicationController
 		@posts = Post.all.order('created_at DESC')
 	end 
 
+	def new 
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
 end 
